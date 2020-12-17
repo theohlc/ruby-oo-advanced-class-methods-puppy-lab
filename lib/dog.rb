621b92044446c585e730b1e94230fa29cdb4acc4
theo <teddyhoover@gmail.com>
2020-12-17 13:01:40 -0600

    commit tree 5cfa87a0ef01bb1f38187f74cf8b918ea928ca1c

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,33 @@
+require 'pry'
 # Add your code here
+class Dog
+    attr_reader :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        self.save
+        #binding.pry
+    end
+
+    def self.all
+        #binding.pry
+        @@all
+    end
+
+    def self.clear_all
+        @@all = []
+    end
+
+    def self.print_all
+        @@all.each do |dog|
+            puts dog.name
+        end
+    end
+
+    def save
+        @@all << self
+    end
+        
+end
